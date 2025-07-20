f3101bc9b89df07c9de1ab5c7e322d23de3e692f
The-Truest-Repairman <brandonpekarek91@gmail.com>
2025-07-20 16:31:28 -0700

    Basic: draw player function

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 # throughout this file
 import pygame
 from constants import *
+import circleshape
+import player
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    #Delta time is the time between frames, used to make sure the game runs at the same speed on all computers    dt = 0
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,8 +24,20 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-
+        #Fill screen with black to start fresh each frame
         screen.fill("black")
+        
+        #Instantiate a new player object and spawn it in the middle of the screen
+        p1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        
+        #Draw the player
+                
+        p1.draw(screen)
+
+        #ENFORCE 60 FPS
+        clock.tick(60)
+        dt = clock.tick()/1000
+
         #Refresh the game screen, this happens at the end!
         pygame.display.flip()
 
