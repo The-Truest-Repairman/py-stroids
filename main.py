4168593991687b5fc30aa65f5d6f95b999d551de
The-Truest-Repairman <brandonpekarek91@gmail.com>
2025-07-20 17:59:31 -0700

    Added: moving asteroids and asteroid field

--- main.py
+++ main.py
@@ -4,18 +4,36 @@
 import pygame
 from constants import *
 import circleshape
-import player
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    #Delta time is the time between frames, used to make sure the game runs at the same speed on all computers    dt = 0
-    dt = 0
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    #Delta time is the time between frames, used to make sure the game runs at the same speed on all computers    dt = 0
+    dt = 0
+
+    #Create new empty groups for objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+     
+    #Add new container to classes to help with organizing the main game loop
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    
+    #Intialization of objects
+    #Instantiate a new player object and spawn it in the middle of the screen
+    p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    a_field = AsteroidField()
 
     #Main game loop
     while True:
@@ -27,16 +45,13 @@ def main():
         #Fill screen with black to start fresh each frame
         screen.fill("black")
         
-        #Instantiate a new player object and spawn it in the middle of the screen
-        p1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        #Enforce 60 FPS
+        dt = clock.tick(60)/1000
         
-        #Draw the player
-                
-        p1.draw(screen)
-
-        #ENFORCE 60 FPS
-        clock.tick(60)
-        dt = clock.tick()/1000
+        #Update objects and then draw them individually
+        updatable.update(dt)       
+        for object in drawable:
+            object.draw(screen)       
 
         #Refresh the game screen, this happens at the end!
         pygame.display.flip()
